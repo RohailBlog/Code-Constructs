13c4949c02179f729b20970dc8243ffc7dea396d
RohailBlog <82020948+RohailBlog@users.noreply.github.com>
2021-04-06 09:15:07 +0100

    Delete Arrays directory

--- Arrays/CC3.c
+++
@@ -1,6 +0,0 @@
-/*This C code is two arrays and nothing is done to them. */
-
-int main() {
-int array[] = {0, 2, 4, 6, 8, 10, 12};
-int array1[] = {3, 4, 5, 63, 2, 65};
-}
--- Arrays/CC5.c
+++
@@ -1,7 +0,0 @@
-/*This C code is three arrays and nothing is done to them. */
-
-int main() {
-int array[] = {220, 102, 2212};
-int array1[] = {12, 33, 59, 1551, 2413};
-int array2[] = {111424, 44244, 5525};
-}
